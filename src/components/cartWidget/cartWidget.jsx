1e57ccbca9be768ef2699c2068c3f8ebdbe69442
Joapaterno <121552748+Joapaterno@users.noreply.github.com>
2023-11-22 00:59:36 -0300

    vs 1.0.9
    vs 1.0.9

--- src/components/cartWidget/cartWidget.jsx
+++ src/components/cartWidget/cartWidget.jsx
@@ -1,15 +1,21 @@
 import { Avatar, Badge } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useShopContext }  from "../shopContext/shopContext";
+
+
 
 const CartWidget = () => {
+
+  const { numProducts } = useShopContext();
+
   return (
     <>
       <card>
-        <Link to={"/cart"} >
-          <Badge count={1}>
+        <Link to="/cart">
+          <Badge count={numProducts}>
             <Avatar shape="square" size="large" icon={<ShoppingCartOutlined />} />
-          </Badge>        
+          </Badge>
         </Link>
       </card>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { CartContextProvider } from './components/Contexto/CartContext.jsx';
 
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-      <App />  
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>    
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
